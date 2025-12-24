9c368ffbe845e83df16a9871ae538488eedffb61
Elimelech Ehrlich <ee525566@gmail.com>
2025-12-24 15:56:10 +0200

    moving middlewares from utils/utils.js to middlewares/midllewares.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,19 +1,1 @@
 
-const requestLogger = (req, res, next) => {
-    console.log(req.method);
-    console.log(req.url);
-    console.log(new Date);
-    next()
-}
-
-const jsonBodyGuard = (req, res, next) => {
-    if ((req.method === "POST" || req.method === "PUT" || req.method === "PATCH") && !req.body) {
-        res.sentStatus(400)        
-    }
-    else next()
-}
-
-export {
-    requestLogger,
-    jsonBodyGuard
-}
