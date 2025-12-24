cab0ad3b364b3918385401ef61f84c4097d63760
Elimelech Ehrlich <ee525566@gmail.com>
2025-12-24 15:45:20 +0200

    add requestLogger middleware in app.use

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express"
 import contactsRouter from "./routes/contactsR.js"
 import notesRouter from "./routes/notesR.js"
+import { requestLogger } from "./utils/utils.js"
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json(), requestLogger)
 
 app.use("/contacts", contactsRouter)
 app.use("/notes", notesRouter)
