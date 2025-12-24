4465afa1a03cc03ba2218c19c1037c53c199b29c
Elimelech Ehrlich <ee525566@gmail.com>
2025-12-24 15:07:08 +0200

    create notesRouter and Preparing the routes in routes/notesR.js

--- routes/notesR.js
+++ routes/notesR.js
@@ -2,11 +2,7 @@ import express from "express"
 
 const router = express.Router()
 
-router.get("/contacts/:id/notes", async (req, res) => {})
-router.get("/:id", async (req, res) => {})
-router.post("/contacts/:id/notes", async (req, res) => {})
-router.put("/:id", async (req, res) => {})
-router.patch("/:id", async (req, res) => {})
+router.get("/:noteId", async (req, res) => {})
 router.delete("/:noteId", async (req, res) => {})
 
 export default router
