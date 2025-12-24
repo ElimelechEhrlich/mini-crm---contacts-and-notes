ff45dd0ebe5d54054f4f76f641a328cacc767755
Elimelech Ehrlich <ee525566@gmail.com>
2025-12-24 15:16:09 +0200

    create `app.use` for `/contacts` and `/notes` in app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from "express"
+import contactsRouter from "./routes/contactsR.js"
+import notesRouter from "./routes/notesR.js"
 
 const app = express()
 
 app.use(express.json())
 
 app.use("/contacts", contactsRouter)
+app.use("/notes", notesRouter)
+
+
 
